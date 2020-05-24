b3c1ab4282e6cfbfc1d3a6b191839f143c7351d6
liyang <liyang@hosecloud.com>
2020-05-24 22:16:48 +0800

    change test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,12 +4,10 @@ describe('webpack.base.js test case', () => {
   console.log(baseConfig)
   it('entry', () => {
     assert.equal(
-      baseConfig.entry.index,
-      '/Volumes/C/baseCli/builder_webpack/test/smoke/template/src/index/index.tsx'
+      baseConfig.entry.index.indexOf('builder_webpack94/test/smoke/template/src/index/index.tsx') > -1, true
     )
     assert.equal(
-      baseConfig.entry.search,
-      '/Volumes/C/baseCli/builder_webpack/test/smoke/template/src/search/index.tsx'
+      baseConfig.entry.search.indexOf('builder_webpack94/test/smoke/template/src/search/index.tsx') > -1 , true
     )
   })
 })
